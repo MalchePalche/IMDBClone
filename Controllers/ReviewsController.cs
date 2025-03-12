6857e95c102be1b9260c7432e302ee5570e052db
Maksim_Palcheff <maxim@palcheff.com>
2025-03-12 12:14:15 +0200

    Controllers Update

---
+++ Controllers/ReviewsController.cs
@@ -0,0 +1,70 @@
+﻿using IMDBClone.Data;
+using IMDBClone.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IMDBClone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Reviews
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Review>>> GetReviews()
+        {
+            return await _context.Reviews.Include(r => r.Movie).Include(r => r.User).ToListAsync();
+        }
+
+        // GET: api/Reviews/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Review>> GetReview(int id)
+        {
+            var review = await _context.Reviews.FindAsync(id);
+
+            if (review == null) return NotFound();
+            return review;
+        }
+
+        // POST: api/Reviews
+        [HttpPost]
+        public async Task<ActionResult<Review>> PostReview(Review review)
+        {
+            _context.Reviews.Add(review);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetReview), new { id = review.Id }, review);
+        }
+
+        // PUT: api/Reviews/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReview(int id, Review review)
+        {
+            if (id != review.Id) return BadRequest();
+
+            _context.Entry(review).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // DELETE: api/Reviews/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReview(int id)
+        {
+            var review = await _context.Reviews.FindAsync(id);
+            if (review == null) return NotFound();
+
+            _context.Reviews.Remove(review);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
