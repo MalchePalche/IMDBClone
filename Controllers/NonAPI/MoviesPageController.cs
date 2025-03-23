6b74cffabbe7ac59f82dc1c33756b1fd04ad3ab2
Maksim_Palcheff <maxim@palcheff.com>
2025-03-23 14:22:14 +0200

    Added Movie page

---
+++ Controllers/NonAPI/MoviesPageController.cs
@@ -0,0 +1,22 @@
+﻿using IMDBClone.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMDBClone.NonAPI 
+{
+    public class MoviesPageController : Controller // ✅ Renamed to avoid conflict
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesPageController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movies.Include(m => m.Genre).ToListAsync();
+            return View(movies);
+        }
+    }
+}
