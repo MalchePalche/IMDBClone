f49d30843ac15b484f8676f0f18e48e9f0e6b223
Maksim_Palcheff <maxim@palcheff.com>
2025-03-25 19:26:29 +0200

    Added genres page

---
+++ Controllers/NonAPI/GenresPageController.cs
@@ -0,0 +1,22 @@
+﻿using IMDBClone.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMDBClone.NonAPI
+{
+    public class GenresPageController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenresPageController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var genres = await _context.Genres.ToListAsync();
+            return View(genres);
+        }
+    }
+}
