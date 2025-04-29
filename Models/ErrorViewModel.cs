382481d4923c34b8fb7c9acac8ceacc8645f7c15
Maksim_Palcheff <maxim@palcheff.com>
2025-04-29 11:04:29 +0300

    Cleanup old project structure after moving to root

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IMDBClone.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
